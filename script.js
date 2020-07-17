2071ff2b6d92662626a83dbdb681a48a30be6b5b
Alex Bertrand <alex.m.bertrand@gmail.com>
2020-07-16 22:32:16 -0400

    start button starts timer, starts quiz js

--- script.js
+++ script.js
@@ -1,87 +1,101 @@
-$(document).ready(function() {
+$(document).ready(function () {
 
-// Setting variables
-const timer = document.getElementById("timeLeft");
-const viewHighScores = document.getElementById("highScores");
-const questionTitle = document.getElementById("questionName");
-const answer1 = document.getElementById("1")
-const answer2 = document.getElementById("2")
-const answer3 = document.getElementById("3")
-const answer4 = document.getElementById("4")
-const questionAnswer = document.getElementById("questionAnswer")
+    // Setting variables
+    let timer = 45;
+    let score = 0;
+    const startBtn = document.querySelector("#startBtn");
+    const timerDisplay = document.querySelector("#timeLeft")
+    const quizQuest = document.querySelector("#questionName");
+    const answer1 = document.getElementById('1');
+    const answer2 = document.getElementById('2');
+    const answer3 = document.getElementById('3');
+    const answer4 = document.getElementById('4');
+    let timerLeft;
 
-// Source https://www.w3schools.com/js/js_quiz.asp
-// Setting questions
-let quizQuestions = [
-    {
-        questionTitle: "How can you add a comment in a Javascript file",
-        answer1: "//This is a comment",
-        answer2: "<!--This is a comment-->",
-        answer3: "'This is a comment",
-        answer4: "$(This is a comment)",
-        correctAnswer: "1"
-    },
-    {
-        questionTitle: "Arrays in Javascript can be used to store _",
-        answer1: "numbers and strings",
-        answer2: "booleans",
-        answer3: "other arrays",
-        answer4: "All of the above",
-        correctAnswer: "4"
-    },
-    {
-        questionTitle: "What does DOM stand for?",
-        answer1: "Developer Office Machine",
-        answer2: "Document Object Model",
-        answer3: "Database Object Module",
-        answer4: "Database Output Model",
-        correctAnswer: "2"
-    },
-    {
-        questionTitle: "Inside which HTML element do we put the Javascript",
-        answer1: "<js>",
-        answer2: "<script>",
-        answer3: "<form>",
-        answer4: "<javascript>",
-        correctAnswer: "2"
-    },
-    {
-        questionTitle: "Commonly used data types DO NOT include:",
-        answer1: "strings",
-        answer2: "booleans",
-        answer3: "alerts",
-        answer4: "numbers",
-        correctAnswer: "3"
-    },
-    {
-        questionTitle: "How many columns are available in a page?",
-        answer1: "15",
-        answer2: "4",
-        answer3: "8",
-        answer4: "12",
-        correctAnswer: "4"
-    },
-    {
-        questionTitle: "What does API stand for?",
-        answer1: "Assisted Python Integration",
-        answer2: "Application Pre Interview",
-        answer3: "Application Programming Interface",
-        answer4: "Assisted Programming Interface",
-        correctAnswer: "3"
-    },
-    {
-        questionTitle: "How does a FOR loop start",
-        answer1: "for(i = 0; i < 5; i++)",
-        answer2: "for(i = 0; i < 5++)",
-        answer3: "for (i <= 5; i+=)",
-        answer4: "for ",
-        correctAnswer: "3"
-    },
-]
+    // Source https://www.w3schools.com/js/js_quiz.asp
+    // Setting questions
+    let quizQuestions = [
+        {
+            questionTitle: "How can you add a comment in a Javascript file",
+            answer1: "//This is a comment",
+            answer2: "<!--This is a comment-->",
+            answer3: "'This is a comment",
+            answer4: "$(This is a comment)",
+            correctAnswer: "1"
+        },
+        {
+            questionTitle: "Arrays in Javascript can be used to store _",
+            answer1: "numbers and strings",
+            answer2: "booleans",
+            answer3: "other arrays",
+            answer4: "All of the above",
+            correctAnswer: "4"
+        },
+        {
+            questionTitle: "What does DOM stand for?",
+            answer1: "Developer Office Machine",
+            answer2: "Document Object Model",
+            answer3: "Database Object Module",
+            answer4: "Database Output Model",
+            correctAnswer: "2"
+        },
+        {
+            questionTitle: "Inside which HTML element do we put the Javascript",
+            answer1: "<js>",
+            answer2: "<script>",
+            answer3: "<form>",
+            answer4: "<javascript>",
+            correctAnswer: "2"
+        },
+        {
+            questionTitle: "Commonly used data types DO NOT include:",
+            answer1: "strings",
+            answer2: "booleans",
+            answer3: "alerts",
+            answer4: "numbers",
+            correctAnswer: "3"
+        },
+        {
+            questionTitle: "How many columns are available in a page?",
+            answer1: "15",
+            answer2: "4",
+            answer3: "8",
+            answer4: "12",
+            correctAnswer: "4"
+        },
+        {
+            questionTitle: "What does API stand for?",
+            answer1: "Assisted Python Integration",
+            answer2: "Application Pre Interview",
+            answer3: "Application Programming Interface",
+            answer4: "Assisted Programming Interface",
+            correctAnswer: "3"
+        },
+        {
+            questionTitle: "How does a FOR loop start",
+            answer1: "for(i = 0; i < 5; i++)",
+            answer2: "for(i = 0; i < 5++)",
+            answer3: "for (i <= 5; i+=)",
+            answer4: "for ",
+            correctAnswer: "3"
+        },
+    ]
 
-$('startBtn').on("click", function() {
-    console.log("Btn clicked");
-})
+    // When start button clicked timer starts countdown and Quiz starts
+    $(startBtn).on("click", function () {
+        // console.log("Button clicked");
+        document.querySelector("#startPage").style.display = "none";
+        document.querySelector("#startQuiz").style.display = "block";
+
+        timerLeft = setInterval(function () {
+            timer--;
+            timerDisplay.innerText = timer + "sec";
+            if (timer <= 0) {
+                clearInterval(timerLeft);
+                // finishedQuiz();
+            }
+        }, 1000)
+    })
 
 });
 
