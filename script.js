615097579c98077e43538ab9c736ea13486d2913
Alex Bertrand <alex.m.bertrand@gmail.com>
2020-07-17 21:02:39 -0400

    debugging

--- script.js
+++ script.js
@@ -6,68 +6,47 @@ $(document).ready(function () {
         {
             questionTitle: "How can you add a comment in a Javascript file",
             choices: ["//This is a comment", "<!--This is a comment-->", "'This is a comment", "$(This is a comment)"],
-            correctAnswer: "0"
+            correctAnswer: 0
         },
         {
             questionTitle: "Arrays in Javascript can be used to store _",
-            choice1: "numbers and strings",
-            choice2: "booleans",
-            choice3: "other arrays",
-            choice4: "All of the above",
-            correctAnswer: "4"
+            choices: ["numbers and strings", "booleans", "other arrays", "All of the above"],
+            correctAnswer: 3
         },
         {
             questionTitle: "What does DOM stand for?",
-            choice1: "Developer Office Machine",
-            choice2: "Document Object Model",
-            choice3: "Database Object Module",
-            choice4: "Database Output Model",
-            correctAnswer: "2"
+            choices: ["Developer Office Machine", "Document Object Model", "Database Object Module", "Database Output Model"],
+            correctAnswer: 1
         },
         {
             questionTitle: "Inside which HTML element do we put the Javascript",
-            choice1: "<js>",
-            choice2: "<script>",
-            choice3: "<form>",
-            choice4: "<javascript>",
-            correctAnswer: "2"
+            choices: ["<js>", "<script>", "<form>", "<javascript>"],
+            correctAnswer: 1
         },
         {
             questionTitle: "Commonly used data types DO NOT include:",
-            choice1: "strings",
-            choice2: "booleans",
-            choice3: "alerts",
-            choice4: "numbers",
-            correctAnswer: "3"
+            choices: ["strings", "booleans", "alerts", "numbers"],
+            correctAnswer: 2
         },
         {
             questionTitle: "How many columns are available in a page?",
-            choice1: "15",
-            choice2: "4",
-            choice3: "8",
-            choice4: "12",
-            correctAnswer: "4"
+            choices: ["15", "4", "8", "12"],
+            correctAnswer: 3
         },
         {
             questionTitle: "What does API stand for?",
-            choice1: "Assisted Python Integration",
-            choice2: "Application Pre Interview",
-            choice3: "Application Programming Interface",
-            choice4: "Assisted Programming Interface",
-            correctAnswer: "3"
+            choices: ["Assisted Python Integration", "Application Pre Interview", "Application Programming Interface", "Assisted Programming Interface"],
+            correctAnswer: 2
         },
         {
             questionTitle: "How does a FOR loop start",
-            choice1: "for(i = 0; i < 5; i++)",
-            choice2: "for(i = 0; i < 5++)",
-            choice3: "for (i <= 5; i+=)",
-            choice4: "for ",
-            correctAnswer: "3"
+            choice1: ["for (i = 0; i < 5; i++)", "for (i = 0; i < 5++)", "for (i <= 5; i+=)", "for i = 1 to 5"],
+            correctAnswer: 0
         },
-    ]
+    ];
+
 
     // Setting variables
-    const startBtn = document.querySelector("#startBtn");
     const timerDisplay = document.querySelector("#timeLeft")
     let timer = 45;
     let score = 0;
@@ -76,11 +55,11 @@ $(document).ready(function () {
 
 
     // When start button clicked timer starts countdown and Quiz starts
-    $(startBtn).on("click", function () {
+    $("#startBtn").on("click", function () {
         // console.log("Button clicked");
         document.querySelector("#startPage").style.display = "none";
         document.querySelector("#startQuiz").style.display = "block";
-        
+
         timerLeft = setInterval(function () {
             timer--;
             timerDisplay.innerText = timer + "sec";
@@ -92,24 +71,25 @@ $(document).ready(function () {
         showQuestions();
     });
 
-    //  Adds question from question array to appear on page and loops through all questions
+    //     //  Adds question from question array to appear on page and loops through all questions
     function showQuestions() {
         $("h3#questionName").text(quizQuestions[questionIndex].questionTitle);
-        for (let i = 0; i < quizQuestions[questionIndex].choices.length; i++ ) {
-            $(".btn-"+ (i + 1)).text(quizQuestions[questionIndex].choices[i])
+        for (let i = 0; i < quizQuestions[questionIndex].choices.length; i++) {
+            $(".btn-" + i).text(quizQuestions[questionIndex].choices[i]);
         }
-    }
+    };
 
-    function checkAnswer() {
-        if(quizQuestions[questionIndex].correctAnswer === $(this).data("index")) {
-            score++;
+    function ClickedAnswer() {
+      if (quizQuestions[questionIndex].correctAnswer === $(this).data("index")) {
+            score++;     
         }
         console.log(score);
-        // questionIndex++;
-        // showQuestions();
+        questionIndex++;
+        console.log("Index =" + questionIndex);
     }
 
-    $(".btn-outline-primary").on("click", checkAnswer);
+
+    $(".answer-button").on("click", ClickedAnswer);
 
 });
 
